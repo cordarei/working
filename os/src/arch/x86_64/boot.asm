348d2e923e5db3213f9340a50c3773f2edba65fa
Joseph Irwin <joseph.irwin.gt@gmail.com>
2016-03-19 17:00:32 +0900

    set up paging

--- os/src/arch/x86_64/boot.asm
+++ os/src/arch/x86_64/boot.asm
@@ -1,13 +1,16 @@
-  global start
+global start
 
-  section .text
-  bits 32
+section .text
+bits 32
 start:
   mov esp, stack_top
 
   call check_multiboot
   call check_cpuid
   call check_longmode
+
+  call set_up_page_tables
+  call enable_paging
 
   mov dword [0xb8000], 0x2f4b2f4f
   hlt
@@ -58,7 +61,53 @@ check_longmode:
   mov al, "2"
   jmp error
 
-  section .bss
+set_up_page_tables:
+  mov eax, p3_table
+  or eax, 0b11
+  mov [p4_table], eax
+  mov eax, p2_table
+  or eax, 0b11
+  mov [p3_table], eax
+
+  mov ecx, 0
+.map_p2_table:
+  mov eax, 0x200000
+  mul ecx
+  or eax, 0b10000011
+  mov [p2_table + ecx*8], eax
+  inc ecx
+  cmp ecx, 512
+  jne .map_p2_table
+
+  ret
+
+enable_paging:
+  mov eax, p4_table
+  mov cr3, eax
+
+  mov eax, cr4
+  or eax, 1<<5
+  mov cr4, eax
+
+  mov ecx, 0xC0000080
+  rdmsr
+  or eax, 1<<8
+  wrmsr
+
+  mov eax, cr0
+  or eax, 1<<31
+  mov cr0, eax
+
+  ret
+
+section .bss
+align 4096
+p4_table:
+  resb 4096
+p3_table:
+  resb 4096
+p2_table:
+  resb 4096
 stack_bottom:
   resb 64
 stack_top:
